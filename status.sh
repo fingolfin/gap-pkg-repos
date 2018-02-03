40956658d51abc2585ca422b292d9f508d03899b
Max Horn <max@quendi.de>
2018-02-03 20:15:24 +0100

    Improve status.sh and update.sh

--- status.sh
+++ status.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
-find . -maxdepth 3 -type d -name .git -execdir bash -c \
-    'echo -e "\e[35m### $(pwd | xargs basename) \e[32m($(git symbolic-ref --short HEAD))\e[0m" && git st && echo' \;
 
-find . -maxdepth 2 -type d -name .hg -execdir bash -c "echo -ne '\e[35m### ' && (pwd | xargs basename) && echo -ne '\e[0m' && hg st && echo" \;
+start=$PWD
+find . -mindepth 2 -maxdepth 3 -name .git -execdir bash -c \
+    'echo -e "\e[35m### ${PWD#'"$start/"'} \e[32m($(git symbolic-ref --short HEAD))\e[0m" && git status && echo' \;
+
+find . -maxdepth 2 -type d -name .hg -execdir bash -c "echo -ne '\e[35m### ' && (pwd | xargs basename) && echo -ne '\e[0m' && hg status && echo" \;
